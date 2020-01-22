9fb0258fad227ff0d1cd6edf804c5358e0ebf01f
Juan Gutierrez <juan.gutierrez@nxp.com>
2020-01-22 15:12:27 -0800

    Initial ota
    Signed-off-by: Juan Gutierrez <juan.gutierrez@nxp.com>

--- ota.c
+++ ota.c
@@ -6,19 +6,31 @@
 
 void main(void)
 {
-	static const char filename[] = "./nextVersion";
+	static const char nextVerFile[] = "./nextVersion";
+	static const char currVerFile[] = "./nextVersion";
 	char verStr[10];
-	float verFloat;
+	float currVerFloat, nextVerFloat;
 	FILE *file;
 
 	/* Check for new version and update nextVersion file*/
 	system("./nextversion.sh");
 	
-	file = fopen(filename, "r");
+	/* Open and read nextVersion file and locally store next version value */
+	file = fopen(nextVerFile, "r");
 	fgets (verStr , 10 , file);
-	sscanf(verStr, "%f", &verFloat);
-	printf("verFloat = %f\n", verFloat);
+	sscanf(verStr, "%f", &nextVerFloat);
+	printf("verFloat = %f\n", nextVerFloat);
+	fclose(file);
 
+	file = fopen(currVerFile, "r");
+	fgets (verStr , 10 , file);
+	sscanf(verStr, "%f", &currVerFloat);
+	printf("verFloat = %f\n", currVerFloat);
+	fclose(file);
+
+	if (nextVerFloat > currVerFloat)
+		printf("OTA pdate is available");
+	
 }
  
 
